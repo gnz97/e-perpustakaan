e13790faca0caaf9ef7d2302d52cc5f7858d7484
gnz97 <risalardan@gmail.com>
2021-10-06 06:18:18 +0700

    update

---
+++ application/views/_partials/js.php
@@ -0,0 +1,20 @@
+ <!-- jQuery -->
+   <script  src="<?=base_url()?>assets/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script  src="<?=base_url()?>assets/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script  src="<?=base_url()?>assets/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script  src="<?=base_url()?>assets/vendors/nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script  src="<?=base_url()?>assets/build/js/custom.min.js"></script>
+
+    <script src="<?=base_url()?>assets/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="<?=base_url()?>assets/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script src="<?=base_url()?>assets/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?=base_url()?>assets/vendors/sweetalert2/sweetalert2.all.min.js"></script>
+
+
+
+    
